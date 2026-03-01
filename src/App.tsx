8a8f8a747db7ddc47164dd0c361fb4ffe943c493
Pachandre <v.andrey-cher@yandex.ru>
2026-03-01 13:55:45 +0300

    some things done

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-	return <>Hello, vega!</>
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,58 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App.tsx";
+import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from "react-router";
+import { StrictMode } from "react";
+import { Provider } from "./components/ui/provider.tsx";
+import Library from "./components/Library.tsx";
+import Project from "./components/project/Project.tsx";
+import Login from "./components/auth/Login.tsx";
+import Register from "./components/auth/Register.tsx";
+import RootRedirect from "./components/RootRedirect.tsx";
+import Preferences from "./components/Preferences.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: (
+      <StrictMode>
+        <Provider>
+          <App />
+        </Provider>
+      </StrictMode>
+    ),
+    children: [
+      {
+        index: true,
+        Component: RootRedirect,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+      {
+        path: "/preferences",
+        Component: Preferences,
+      },
+      {
+        path: "/projects",
+        Component: Library,
+      },
+      {
+        path: "/project/:uuid",
+        loader: async () => {
+          return { res: await fetch("https://httpbin.org/delay/5") };
+        },
+        Component: Project,
+      },
+    ],
+  },
+]);
+
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
