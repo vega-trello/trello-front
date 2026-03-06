3f715a51f708849563052fbc5a768b7ae964d8e6
Pachandre <v.andrey-cher@yandex.ru>
2026-03-06 11:52:36 +0300

    fix gh pages?

--- src/main.tsx
+++ src/main.tsx
@@ -17,41 +17,36 @@ import RequireAuth from "./middleware/requireAuth.tsx";
 const createRouter =
   import.meta.env.MODE === "gh-pages" ? createHashRouter : createBrowserRouter;
 
-const router = createRouter(
-  [
-    {
-      element: (
-        <StrictMode>
-          <Provider>
-            <App />
-          </Provider>
-        </StrictMode>
-      ),
-      children: [
-        { index: true, Component: RootRedirect },
-        { path: "/login", Component: Login },
-        { path: "/register", Component: Register },
-        {
-          element: <RequireAuth />,
-          children: [
-            { path: "/preferences", Component: Preferences },
-            { path: "/projects", Component: Projects },
-            {
-              path: "/project/:uuid",
-              Component: Project,
-              loader: async ({ params }) => {
-                return await API.GetProject(params["uuid"] as string);
-              },
-            },
-          ],
-        },
-      ],
-    },
-  ],
+const router = createRouter([
   {
-    basename: import.meta.env.BASE_URL,
+    element: (
+      <StrictMode>
+        <Provider>
+          <App />
+        </Provider>
+      </StrictMode>
+    ),
+    children: [
+      { index: true, Component: RootRedirect },
+      { path: "/login", Component: Login },
+      { path: "/register", Component: Register },
+      {
+        element: <RequireAuth />,
+        children: [
+          { path: "/preferences", Component: Preferences },
+          { path: "/projects", Component: Projects },
+          {
+            path: "/project/:uuid",
+            Component: Project,
+            loader: async ({ params }) => {
+              return await API.GetProject(params["uuid"] as string);
+            },
+          },
+        ],
+      },
+    ],
   },
-);
+]);
 
 const root = document.getElementById("root")!;
 
