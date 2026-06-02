5305ebad576668209790238615efaeb7e3ec25c8
Pachandre <v.andrey-cher@yandex.ru>
2026-06-02 22:46:21 +0300

    fix: gh-pages

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,6 +3,6 @@ import { Adapter as BackendAdapter } from "./adapters/backend";
 import { Adapter as LocalStorageAdapter } from "./adapters/frontend";
 
 export const API: APIAdapter =
-	import.meta.env.MODE === "test" || import.meta.env.PROD
+	import.meta.env.MODE === "test" || import.meta.env.PROD || import.meta.env.MODE == 'gh-pages'
 		? BackendAdapter
 		: LocalStorageAdapter;
