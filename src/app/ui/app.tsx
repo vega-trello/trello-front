0f5ee6f6bff8a4e8934ee08d159a3e4038f4ff38
Pachandre <v.andrey-cher@yandex.ru>
2026-06-03 15:15:49 +0300

    fix(app): css import

--- src/app/ui/app.tsx
+++ src/app/ui/app.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
-import "./App.css";
 import { Header } from "../../widgets";
 import { ToasterRoot } from "../../shared";
+import './app.css'
 
 export function App() {
 	return (
